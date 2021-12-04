56f5a685204124df1471f06bc42710e11ba8c10c
dangerdrennan <pdrennan90@gmail.com>
2021-12-04 00:29:43 -0500

    db test

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: 'process.env.DATABASE_URL || postgresql://postgres:Mushr00mM@rio@localhost:5432/cams',
+  
 });
 
 
@@ -22,7 +20,7 @@ express()
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
+      const result = await client.query('SELECT * FROM prof');
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
       client.release();
